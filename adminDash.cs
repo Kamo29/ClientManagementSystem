5309da3e98e4ff3b2cd581e2d99f2c67abb91aa1
Kamogelo Phiri <140900860+Kamo29@users.noreply.github.com>
2025-10-20 10:55:03 +0200

    Create adminDash.cs

---
+++ adminDash.cs
@@ -0,0 +1,58 @@
+ public partial class adminDash : Form
+ {
+     private int id;
+     private string username, password;
+
+     private DataGridView dt;
+
+     public adminDash()
+     {
+         InitializeComponent();
+     }
+
+     private void bttnDelete_Click(object sender, EventArgs e)
+     {
+         AgentData agentData = new AgentData();
+         agentData.DeleteAgent(id);
+
+         ClientData clients = new ClientData();
+         clients.DeleteClient(id);
+     }
+
+     private void LoadAgentData()
+     {
+         AgentData agentData = new AgentData();
+         //DataTable agentsTable = agentData.getAgents();
+
+         dt.DataSource = agentData.getAgents();
+     }
+
+     private void adminDash_Load(object sender, EventArgs e)
+     {
+         dt = new DataGridView();
+         this.Controls.Add(dt);
+
+         dt.Dock = DockStyle.Fill;
+
+         LoadAgentData();
+     }
+
+     private void bttnSearch_Click(object sender, EventArgs e)
+     {
+         AgentData agentData = new AgentData();
+         agentData.SearchAgentById(id);
+
+         dt.DataSource = agentData;
+     }
+
+     private void agentsDataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+     {
+
+     }
+
+     private void bttnAdd_Click(object sender, EventArgs e)
+     {
+         UserData ud = new UserData();
+         ud.addAgent(username, password);
+     }
+ }
