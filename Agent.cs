eb182c7a4041cd5514ce367f819699cba03f338c
Kamogelo Phiri <140900860+Kamo29@users.noreply.github.com>
2025-10-20 11:01:12 +0200

    Add Agent class with RoleID property

---
+++ Agent.cs
@@ -0,0 +1,15 @@
+namespace ClientManagementSystem.Logic
+{
+    internal class Agent
+    {
+        public int RoleID { get; set; }
+
+        public Agent()
+        { }
+
+        public Agent(int roleID)
+        {
+            RoleID = roleID;
+        }
+    }
+}
