f6c8d3f9615566162d17094b04a7a61309a0e605
Kamogelo Phiri <140900860+Kamo29@users.noreply.github.com>
2025-10-20 11:06:23 +0200

    Refactor adminDash class structure and methods

--- adminDash.cs
+++ adminDash.cs
@@ -1,58 +1,66 @@
- public partial class adminDash : Form
- {
-     private int id;
-     private string username, password;
+ namespace ClientManagementSystem.Presentation
+{
+    public partial class adminDash : Form
+    {
+        private int id;
+        private string username, password;
 
-     private DataGridView dt;
+        private DataGridView dt;
 
-     public adminDash()
-     {
-         InitializeComponent();
-     }
+        public adminDash()
+        {
+            InitializeComponent();
+        }
 
-     private void bttnDelete_Click(object sender, EventArgs e)
-     {
-         AgentData agentData = new AgentData();
-         agentData.DeleteAgent(id);
+        private void bttnDelete_Click(object sender, EventArgs e)
+        {
+            AgentData agentData = new AgentData();
+            agentData.DeleteAgent(id);
 
-         ClientData clients = new ClientData();
-         clients.DeleteClient(id);
-     }
+            ClientData clients = new ClientData();
+            clients.DeleteClient(id);
+        }
 
-     private void LoadAgentData()
-     {
-         AgentData agentData = new AgentData();
-         //DataTable agentsTable = agentData.getAgents();
+        private void LoadAgentData()
+        {
+            AgentData agentData = new AgentData();
+            //DataTable agentsTable = agentData.getAgents();
 
-         dt.DataSource = agentData.getAgents();
-     }
+            dt.DataSource = agentData.getAgents();
+        }
 
-     private void adminDash_Load(object sender, EventArgs e)
-     {
-         dt = new DataGridView();
-         this.Controls.Add(dt);
+        private void adminDash_Load(object sender, EventArgs e)
+        {
+            dt = new DataGridView();
+            this.Controls.Add(dt);
 
-         dt.Dock = DockStyle.Fill;
+            dt.Dock = DockStyle.Fill;
 
-         LoadAgentData();
-     }
+            LoadAgentData();
+        }
 
-     private void bttnSearch_Click(object sender, EventArgs e)
-     {
-         AgentData agentData = new AgentData();
-         agentData.SearchAgentById(id);
+        private void bttnSearch_Click(object sender, EventArgs e)
+        {
+            AgentData agentData = new AgentData();
+            agentData.SearchAgentById(id);
 
-         dt.DataSource = agentData;
-     }
+            dt.DataSource = agentData;
+        }
 
-     private void agentsDataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
-     {
+        private void agentsDataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
-     }
+        }
 
-     private void bttnAdd_Click(object sender, EventArgs e)
-     {
-         UserData ud = new UserData();
-         ud.addAgent(username, password);
-     }
- }
+        private void bttnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnAdd_Click(object sender, EventArgs e)
+        {
+            UserData ud = new UserData();
+            ud.addAgent(username, password);
+        }
+    }
+}
