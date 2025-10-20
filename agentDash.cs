8cb587312cfa0e7fd51eaf5763b98ae8793bce4d
Kamogelo Phiri <140900860+Kamo29@users.noreply.github.com>
2025-10-20 11:03:11 +0200

    Refactor agentDash class and restructure code

--- agentDash.cs
+++ agentDash.cs
@@ -1,59 +1,67 @@
- public partial class agentDash : Form
- {
-     private string name, address, email, accountHistory, phoneNumber;
-     private int id;
+ namespace ClientManagementSystem.Presentation
+{
+    public partial class agentDash : Form
+    {
+        private string name, address, email, accountHistory, phoneNumber;
+        private int id;
 
-     private DataGridView dt;
-     private DataTable table;
+        private DataGridView dt;
+        private DataTable table;
 
-     private void LoadClientData()
-     {
-         ClientData clientData = new ClientData();
+        private void LoadClientData()
+        {
+            ClientData clientData = new ClientData();
 
-         clientsDataGrid.DataSource = clientData.getClients();
-     }
-     
-     private void bttnSearch_Click(object sender, EventArgs e)
-     {
-         ClientData client = new ClientData();
+            clientsDataGrid.DataSource = clientData.getClients();
+        }
+        
+        private void bttnSearch_Click(object sender, EventArgs e)
+        {
+            ClientData client = new ClientData();
 
-         clientsDataGrid.DataSource = client.SearchClientById(id);
+            clientsDataGrid.DataSource = client.SearchClientById(id);
 
-     }
+        }
 
-     private void agentDash_Load(object sender, EventArgs e)
-     {
-         ClientData client = new ClientData();
-         this.Controls.Add(dt);
+        private void agentDash_Load(object sender, EventArgs e)
+        {
+            ClientData client = new ClientData();
+            this.Controls.Add(dt);
 
-         dt.Dock = DockStyle.Fill;
+            dt.Dock = DockStyle.Fill;
 
-         LoadClientData();
-     }
+            LoadClientData();
+        }
 
-     private void bttnUpdate_Click(object sender, EventArgs e)
-     {
-         ClientData client = new ClientData();
-         client.UpdateClient(id, name, address, phoneNumber, email, accountHistory);
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
-         
-     }
+        }
 
-     public agentDash()
-     {
-         InitializeComponent();
-         
-     }
+        private void bttnUpdate_Click(object sender, EventArgs e)
+        {
+            ClientData client = new ClientData();
+            client.UpdateClient(id, name, address, phoneNumber, email, accountHistory);
 
-     private void bttnDelete_Click(object sender, EventArgs e)
-     {
+            
+        }
 
-     }
+        public agentDash()
+        {
+            InitializeComponent();
+            
+        }
 
-     private void bttnAdd_Click(object sender, EventArgs e)
-     {
-         ClientData client = new ClientData();
-         client.AddClient(name, address, phoneNumber, email, accountHistory);
-     }
-     
- }
+        private void bttnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnAdd_Click(object sender, EventArgs e)
+        {
+            ClientData client = new ClientData();
+            client.AddClient(name, address, phoneNumber, email, accountHistory);
+        }
+        
+    }
+}
